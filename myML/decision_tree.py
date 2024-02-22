93b209a9a2ab9c9e20b4fd57dfcda4f5799f785f
yyhyan100 <yyhyan@gmail.com>
2024-02-21 20:45:39 -0600

    ss

--- myML/decision_tree.py
+++ myML/decision_tree.py
@@ -2,12 +2,15 @@ import numpy as np
 from math import *
 
 class tree:
-	node_id=-1
-	subtree=dict()
-	leaf_value=None
+    def __init__(self):
+        self.node_id=-1
+        self.subtree={}
+        self.leaf_value=None
 
 class classification_tree:
-    trained_tree=tree()
+    def __init__(self):
+        self.trained_tree=tree()
+
     def __get_entropy(self,y):
         n=y.size
         results,counts=np.unique(y, return_counts=True)
@@ -41,12 +44,13 @@ class classification_tree:
         self.features=list(range(n))
         self.record_num=m
         self.hd=self.__get_entropy(y_train)
-        self.tree=self.__build_tree(X_train,y_train,self.features)
+        self.trained_tree=self.__build_tree(X_train,y_train,self.features)
 
     def __build_tree(self,X,y,selector=[]):
         newtree = tree()
         values_y, counts = np.unique(y, return_counts=True)
         if values_y.size==1 or len(selector)==0:
+            print("leaf created:",values_y[np.argmax(counts)])
             newtree.leaf_value=values_y[np.argmax(counts)]
             return newtree
 
@@ -55,6 +59,7 @@ class classification_tree:
             values=np.unique(X[:,0])
             if values.size==1: 
                 newtree.leaf_value=values_y[np.argmax(counts)]
+                print("leaf created:",values_y[np.argmax(counts)])
                 return newtree
             else:
                 newtree.node_id=selector[0]
@@ -68,7 +73,7 @@ class classification_tree:
         for j in range(len(selector)):
             hda=self.__get_cond_entropy(X[:,j],y)
             had=self.__get_feature_entropy(X[:,j])
-            gain_ratio[j]=(self.hd-hda)/had
+            gain_ratio[j]=(hd-hda)/had
         id=np.argmax(gain_ratio)
         newtree.node_id=selector[id]
         values=np.unique(X[:,id])
@@ -76,9 +81,10 @@ class classification_tree:
         new_cols=[i for i in range(len(selector)) if i!=id]
         print(new_selector)
         print(values)
-
+        print("now next:")
         for i in range(values.size):
             subidx = X[:,id]==values[i]
+            print("subtree:",values[i])
             print(X[:,new_cols][subidx])
             newtree.subtree[values[i]]=self.__build_tree(X[:,new_cols][subidx],y[subidx],new_selector)  
         return newtree
@@ -92,10 +98,10 @@ class classification_tree:
 
     def predict(self,X_predict):
         if len(X_predict.shape)==1 :
-            result=self.__find_leaf(self.tree,X_predict)
+            result=self.__find_leaf(self.trained_tree,X_predict)
         else:
             m=X_predict.shape[0]
             result=np.empty(m)
             for i in range(m):
-                result[i]=self.__find_leaf(self.tree,X_predict[i])
+                result[i]=self.__find_leaf(self.trained_tree,X_predict[i])
         return result
