f1277b541e3c4f260ed56283ea1854c28f456a6c
yyhyan100 <yyhyan@gmail.com>
2024-04-25 19:20:08 -0500

    ann

---
+++ myML/ANN.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+class ANN_Reg:
+    def __init__(self, num_hidden_layers=(100, 100), eta=0.01, max_iter=1000, tol=0.0001):
+        self.num_hlayers = num_hidden_layers
+        self.eta = eta
+        self.max_iter = max_iter
+        self.tol = tol
+
+    def __sigmoid(self, z):
+        return 1.0 / (1.0 + np.exp(-z))
+
+    def __get_z(self, x, W):
+        return np.matmul(x, W)
+
+    def __get_error(self, y, y_predict):
+        return np.sum((y - y_predict) ** 2) / len(y)
+        
+    def __backpropagation(self, X, y):
+        m, n = X.shape
+        _, n_out = y.shape
+
+        layer_sizes = self.num_hlayers + (n_out,)
+        layer_n = len(layer_sizes)
+
+        W = []
+        li_size = n
+        for lj_size in layer_sizes:
+            Wij = np.random.rand(li_size + 1, lj_size) * 0.05
+            W.append(Wij)
+            li_size = lj_size
+
+        in_list      = [None] * layer_n
+        z_list       = [None] * layer_n
+        out     = [None] * layer_n
+        delta   = [None] * layer_n
+
+        idx = np.arange(m)
+        for _ in range(self.max_iter):
+            np.random.shuffle(idx)
+            X, y = X[idx], y[idx]
+
+            for x, t in zip(X, y):
+                out = x
+                for i in range(layer_n):
+                    in_ = np.ones(out.size + 1)
+                    in_[1:] = out 
+                    z = self.__get_z(in_, W[i])
+                    if i != layer_n - 1:
+                        out = self.__sigmoid(z)
+                    else:
+                        out = z
+                    in_list[i], z_list[i], out[i] = in_, z, out
+
+
+                delta[-1] = t - out
+                for i in range(layer_n - 2, -1, -1):
+                    out_i, W_j, delta_j = out[i], W[i+1], delta[i+1]
+                    delta[i] = out_i * (1. - out_i) * np.matmul(W_j[1:], delta_j[:, None]).T[0]
+
+                for i in range(layer_n):
+                    in_i, delta_i = in_list[i], delta[i]
+                    W[i] += in_i[:, None] * delta_i * self.eta
+
+            y_pred = self.__forward(X, W)
+            err = self.__get_error(y, y_pred)
+
+            if err < self.tol:
+                break
+
+        return W
+
+    def train(self, X, y):
+        self.W = self.__backpropagation(X, y)
+
+    def __forward(self, X, W):
+        layer_n = len(W)
+
+        out = X
+        for i in range(layer_n):
+            m, n = out.shape
+            in_ = np.ones((m, n + 1))
+            in_[:, 1:] = out
+            z = self.__get_z(in_, W[i])
+            if i != layer_n - 1:
+                out = self.__sigmoid(z)
+            else:
+                out = z
+
+        return out
+
+    def predict(self, X):
+        return self.__forward(X, self.W)
--- myML/PCA.py
+++ myML/PCA.py
@@ -1,8 +1,32 @@
 import numpy as np
+class pca:
+    L=None
+    s=None
+    R=None
+    __trained=False
+    def train(self,U):
+        self.m,self.n=U.shape
+        self.L,self.s,self.R=np.linalg.svd(U)
+        self.A=self.L*self.s
+        self.__trained=True
+
+    def get_modes(self,n=0):
+        if n==0 : n=self.m
+        if self.__trained : return self.R[:,:n]
+
+    def get_coeff(self,n=0):
+        if n==0 : n=self.m
+        if self.__trained : return self.A[:n,:]
+
+    def get_eigenvalues(self,n=0):
+        if n==0 : n=self.m
+        if self.__trained : return self.s[:n]**2
+
 class pac_snapshot:
     L=None
     s=None
     R=None
+    __trained=False
     def train(self,U):
         self.m,self.n=U.shape
         self.L,self.s,self.R=np.linalg.svd(U)
@@ -10,12 +34,12 @@ class pac_snapshot:
 
     def get_modes(self,n=0):
         if n==0 : n=self.m
-        if self.L != None : return self.A[:,:n]
+        if self.__trained: return self.A[:,:n]
 
     def get_coeff(self,n=0):
         if n==0 : n=self.m
-        if self.L != None : return self.R[:n,:]
+        if self.__trained : return self.R[:n,:]
 
     def get_eigenvalues(self,n=0):
         if n==0 : n=self.m
-        if self.L != None : return self.s[:n]**2
+        if self.__trained : return self.s[:n]**2
